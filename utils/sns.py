80672111e60e056731c70d7a76241b72c6dfc237
augustonascimentos <augustonascimentos@gmail.com>
2021-06-09 11:06:50 -0300

    adding sns log

--- utils/sns.py
+++ utils/sns.py
@@ -1,4 +1,5 @@
 import boto3
+from utils import logger
 
 
 class Sns:
@@ -7,5 +8,6 @@ class Sns:
         self.sns_client = None
 
     def sns_client_connection(self):
+        logger.info('Initializing Sns client')
         self.sns_client = boto3.client('sns')
         return self.sns_client
